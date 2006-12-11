c4bae3bd810bf932f7eef8743a44a560540ab835
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2006-12-11 15:55:06 +0000

    fixed web tests, fixes #1344

--- modules/default/tests/ScreensLoadWithoutErrorsTest.web.php
+++ modules/default/tests/ScreensLoadWithoutErrorsTest.web.php
@@ -13,15 +13,15 @@ class DefaultScreensLoadWithoutErrorsTest extends WebTestCase
     {
         $this->addHeader('User-agent: foo-bar');
         $this->get($this->conf['site']['baseUrl']);
-        $this->assertTitle('Seagull Framework :: Home');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Home');
         $this->assertNoUnwantedPattern("/errorContent/");
 
         $this->get($this->conf['site']['baseUrl'] . '/index.php/user/register/');
-        $this->assertTitle('Seagull Framework :: Register');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Register');
         $this->assertNoUnwantedPattern("/errorContent/");
 
         $this->get($this->conf['site']['baseUrl'] . '/index.php/default/bug/');
-        $this->assertTitle('Seagull Framework :: Bug Report');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Bug Report');
         $this->assertNoUnwantedPattern("/errorContent/");
     }
 
@@ -35,25 +35,25 @@ class DefaultScreensLoadWithoutErrorsTest extends WebTestCase
 #        $this->showSource();
 
         //  modules
-        $this->assertTitle('Seagull Framework :: Module Manager');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Module Manager');
         $this->assertNoUnwantedPattern("/errorContent/");
 
         $this->clickLink('Manage');
-        $this->assertTitle('Seagull Framework :: Module Manager');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Module Manager');
         $this->assertNoUnwantedPattern("/errorContent/");
 
         $this->clickLink('Configuration');
-        $this->assertTitle('Seagull Framework :: Config Manager');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Config Manager');
         $this->assertNoUnwantedPattern("/errorContent/");
 
         // bug mgr
         $this->get($this->conf['site']['baseUrl'] . '/index.php/default/bug/');
-        $this->assertTitle('Seagull Framework :: Bug Report');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Bug Report');
         $this->assertNoUnwantedPattern("/errorContent/");
 
         //  mtce
         $this->get($this->conf['site']['baseUrl'] . '/index.php/default/maintenance/');
-        $this->assertTitle('Seagull Framework :: Maintenance');
+        $this->assertTitle($this->conf['site']['name'] . ' :: Maintenance');
         $this->assertNoUnwantedPattern("/errorContent/");
     }
 }
