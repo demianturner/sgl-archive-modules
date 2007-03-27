27852bf8aead322ae541495079a068c24fb390c4
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-03-27 12:16:59 +0000

    renaming blog module to s9ywrapper -1

--- modules/s9ywrapper/data/data.default.my.sql
+++ modules/s9ywrapper/data/data.default.my.sql
@@ -1,1 +1,1 @@
-INSERT INTO module VALUES ({SGL_NEXT_ID}, 1, 'blog', 'Blog', 'The blog module is a wrapper for the Serendipity blog software, see http://trac.seagullproject.org/wiki/Integration/Serendipity for info how to install.', NULL, 'module_default.gif', '', NULL, NULL, NULL);
+INSERT INTO module VALUES ({SGL_NEXT_ID}, 1, 's9ywrapper', 'Serendipity Wrapper', 'The s9ywrapper module is a wrapper for the Serendipity blog software, see http://trac.seagullproject.org/wiki/Integration/Serendipity for info how to install.', NULL, 'module_default.gif', '', NULL, NULL, NULL);
