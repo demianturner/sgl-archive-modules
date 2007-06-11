3256e9b55d131b8682fb5f573413341d50c4802f
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-06-11 13:34:04 +0000

    versioning modules

--- modules/comment/data/data.default.pg.sql
+++
@@ -1,5 +0,0 @@
--- Last edited: Antonio J. Garcia 2007-04-24
--- leave subqueries on a single line in order that table prefixes works
-BEGIN;
-INSERT INTO module VALUES ({SGL_NEXT_ID}, 1, 'comment', 'Comments', 'Allows users to associate comments with any module in the system.', '', '48/module_block.png', '', NULL, NULL, NULL);
-COMMIT;
