1e6317725d140b08ef100c95bc02330ac3aa6252
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-06-26 15:26:45 +0000

    implementing persistent logins -1

--- modules/0.6/default/lang/english-iso-8859-15.php
+++ modules/0.6/default/lang/english-iso-8859-15.php
@@ -554,6 +554,7 @@ $defaultWords['translations'] = 'translations';
 $defaultWords['Enter Captcha'] = 'Please enter the number shown below in the relevant field';
 $defaultWords['You must enter the number in this field'] = 'You must enter the number in this field';
 $defaultWords['prefixes not supported'] = 'Currently the module generator only works when there are no prefixes set on the db tables';
+$defaultWords['Enable persistent logins'] = 'Enable persistent logins';
 
 /* --- PEAR::Pager --- */
 $defaultWords['altPrev'] = 'Previous';
