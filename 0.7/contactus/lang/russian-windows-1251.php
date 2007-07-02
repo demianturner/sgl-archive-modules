4564726d277d0bf6f3ceedeb96062d007879f456
Demian Turner <demian@seagullsystems.com>
2007-07-02 15:52:22 +0000

    adding admin screens to contactus module -1, fixes #1038

---
+++ 0.7/contactus/lang/russian-windows-1251.php
@@ -0,0 +1,57 @@
+<?php
+    
+$words = array(
+   
+/* ContactUsMgr */
+
+    // titles
+    'Contact Us' => 'Обратная связь',
+        
+    // list
+    'contact intro'   => 'Чтобы связаться с нами, пожалуйста, заполните поля формы, указав личные данные, тип вопроса и вопрос.',
+    'First Name'      => 'Имя',
+    'Last Name'       => 'Фамилия',
+    'Email'           => 'Эл. почта',
+    'Type of enquiry' => 'Тип вопроса',
+    'Comment'         => 'Вопрос',
+    'Enter Captcha'   => 'Пожалуйста, укажите номер, показанный ниже, в соответствующем поле формы',
+        
+    'aContactType' => array(
+        'General enquiry' => 'Вопрос на общую тему',
+        'Get a quote'     => 'Получение квоты',
+        'Hosting info'    => 'Информация о хостинг-услугах',
+        'Site feedback'   => 'Обратная взязь',
+    ),
+
+    // validation
+    'You must enter your first name'          => 'Пожалуйста, укажите имя',
+    'You must enter your last name'           => 'Пожалуйста, укажите фамилию',
+    'You must fill in your comment'           => 'Пожалуйста, укажите вопрос',
+    'You must enter your email'               => 'Пожалуйста, укажите адрес эл. почты',
+    'Your email is not correctly formatted'   => 'Адрес эл. почты не корректен',
+    'You must enter the number in this field' => 'Вы должны ввести число в этом поле',
+        
+    // email
+    'Contact Enquiry from' => 'Вопрос от',
+
+        
+/* ContactUsAdminMgr */
+
+    // titles
+    'Contact us\' manager' => 'Управление разделом "Обратная связь"',
+    'Comments list'        => 'Список комментариев',
+    
+    // list
+    'Be careful!' => 'Осторожно!',
+    'Log time'    => 'Время отсылки',
+    
+    // view
+    'Back to list'                                       => 'Назад к списку',
+    'Comment details'                                    => 'Описание комментария',
+    'View comment on %logtime by %first_name %last_name' => 'Просмотр комментария %logtime, %first_name %last_name',
+    
+    // messages
+    'Selected comments successfully deleted' => 'Выбранные комментарии успешно удалены'
+);
+
+?>
