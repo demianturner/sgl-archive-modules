e20177b122639a5a1e7a01fdd61870154401a6c0
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-08-23 17:30:23 +0000

    Session files can now be saved whereever you like

--- modules/0.6/default/lang/english-iso-8859-15.php
+++ modules/0.6/default/lang/english-iso-8859-15.php
@@ -376,6 +376,7 @@ Author: Rafael Ferreira Silva <rafael@webphp.com.br>
         'Custom output class' => 'Custom output class',
         'Show errors' => 'Show errors',
         'Select No if you do not want errors displayed in the logs' => 'Select No if you do not want errors displayed in the logs',
+        'Save path for session file' => 'Save path for session file',
 
 /*
  ConfigMgr: MTA options
