4ffa1840daba3a0805f6b4bd9a4415d6afeb2365
lakiboy <lakiboy@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-08-28 16:09:47 +0000

    added new translation modules to repo

---
+++ modules/0.6/translation/data/navigation.php
@@ -0,0 +1,15 @@
+<?php
+$aSections = array(
+    array (
+        'title'         => 'Manage Translations',
+        'parent_id'     => SGL_NODE_ADMIN,
+        'uriType'       => 'dynamic',
+        'module'        => 'translation',
+        'manager'       => 'Translation2Mgr.php',
+        'actionMapping' => '',
+        'add_params'    => '',
+        'is_enabled'    => 1,
+        'perms'         => 'SGL_ADMIN',
+    )
+);
+?>
