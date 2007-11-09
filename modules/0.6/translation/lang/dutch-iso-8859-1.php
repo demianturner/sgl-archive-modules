33c805cb507ec871b971e39a60bcc1ce0faaf289
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2007-11-09 17:14:21 +0000

    Fixed prob where global and module configs were saving in global file -1

---
+++ modules/0.6/translation/lang/dutch-iso-8859-1.php
@@ -0,0 +1,7 @@
+<?php
+$words['__SGL_UPDATED_BY'] = 'admin';
+$words['__SGL_UPDATED_BY_ID'] = '1';
+$words['__SGL_LAST_UPDATED'] = '2007-10-17 20:11:22';
+$words['Last modified by %user on %date'] = 'Voor het laatst gewijzigd door %user op %date';
+$words['Language'] = 'Taal';
+?>
