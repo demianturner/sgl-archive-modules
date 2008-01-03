2c87e6c66f375401ee4f1857ff54803ee3819019
Dmitri Lakachauskis <lakiboy83@gmail.com>
2008-01-03 08:42:06 +0000

    record login when remember me feature is activated

--- 0.6/user/classes/observers/RecordLogin.php
+++ 0.6/user/classes/observers/RecordLogin.php
@@ -1,21 +1,68 @@
 <?php
+
+/**
+ * Records login in database.
+ *
+ * @package seagull
+ * @subpackage user
+ * @author Demian Turner <demian@phpkitchen.com>
+ * @author Dmitri Lakachauskis <lakiboy83@gmail.com>
+ */
 class RecordLogin extends SGL_Observer
 {
+    /**
+     * Observer's routine.
+     *
+     * @access public
+     *
+     * @param SGL_Observable $observable
+     */
     function update($observable)
     {
-        // Get the user id from the current session
-        $uid = SGL_Session::getUid();
-        //  record login in db for security
-        if (@$observable->conf['LoginMgr']['recordLogin']) {
-
-            require_once 'DB/DataObject.php';
-            $login = DB_DataObject::factory($observable->conf['table']['login']);
-            $login->login_id = $observable->dbh->nextId($observable->conf['table']['login']);
-            $login->usr_id = $uid;
-            $login->date_time = SGL_Date::getTime(true);
-            $login->remote_ip = $_SERVER['REMOTE_ADDR'];
-            $ok = $login->insert();
+        // record login in db for security
+        if (RecordLogin::loginRecordingAllowed()) {
+            RecordLogin::insert($observable->dbh);
         }
+    }
+
+    /**
+     * Checks if it is allowed to record login.
+     *
+     * @static
+     *
+     * @access public
+     *
+     * @return boolean
+     */
+    function loginRecordingAllowed()
+    {
+        $c = &SGL_Config::singleton();
+        $conf = $c->load(SGL_MOD_DIR . '/user/conf.ini');
+        return (boolean) $conf['LoginMgr']['recordLogin'];
+    }
+
+    /**
+     * Insert record into database.
+     *
+     * @static
+     *
+     * @access public
+     *
+     * @param DB_Common $dbh
+     * @param integer $userId
+     *
+     * @return boolean
+     */
+    function insert(&$dbh, $userId = null)
+    {
+        require_once 'DB/DataObject.php';
+        $tableName        = SGL_Config::get('table.login');
+        $login            = DB_DataObject::factory($tableName);
+        $login->login_id  = $dbh->nextId($tableName);
+        $login->usr_id    = $userId ? $userId : SGL_Session::getUid();
+        $login->date_time = SGL_Date::getTime(true);
+        $login->remote_ip = $_SERVER['REMOTE_ADDR'];
+        return $login->insert();
     }
 }
 ?>
