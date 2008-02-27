40b983cc926a2522a74c8264cb5652c6913b6157
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-02-27 11:25:40 +0000

    updated dutch translations

--- modules/0.6/translation/lang/english-iso-8859-15.php
+++ modules/0.6/translation/lang/english-iso-8859-15.php
@@ -1,6 +1,7 @@
 <?php
 $words = array(
 	'Last modified by %user on %date' => 'Last modified by %user on %date',
-	'Language' => 'Language',
+    'Language' => 'Language',
+	'Show only untranslated strings' => 'Show only untranslated strings',
 );
 ?>
