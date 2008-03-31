664a119eb9e50118451cc323a4a856927e24641f
lakiboy <lakiboy@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-03-31 14:27:13 +0000

    added navigation branch for translators

--- modules/0.6/translation/data/navigation.php
+++ modules/0.6/translation/data/navigation.php
@@ -9,7 +9,7 @@ $aSections = array(
         'actionMapping' => '',
         'add_params'    => '',
         'is_enabled'    => 1,
-        'perms'         => 'SGL_ADMIN',
+        'perms'         => 'SGL_ADMIN,SG',
     ),
     array(
         'title'         => 'Summary',
@@ -21,6 +21,33 @@ $aSections = array(
         'add_params'    => '',
         'is_enabled'    => 1,
         'perms'         => 'SGL_ADMIN',
+    ),
+
+    /**
+     * @todo add SGL_TRANSLATOR constant to init.php
+     * @todo add SGL_NODE_TRANSLATOR constant to init.php
+     */
+    array(
+        'title'         => 'Manage Translations',
+        'parent_id'     => 5,
+        'uriType'       => 'dynamic',
+        'module'        => 'translation',
+        'manager'       => 'TranslationMgr.php',
+        'actionMapping' => '',
+        'add_params'    => '',
+        'is_enabled'    => 1,
+        'perms'         => 3,
+    ),
+    array(
+        'title'         => 'Summary',
+        'parent_id'     => SGL_NODE_GROUP,
+        'uriType'       => 'dynamic',
+        'module'        => 'translation',
+        'manager'       => 'TranslationMgr.php',
+        'actionMapping' => 'summary',
+        'add_params'    => '',
+        'is_enabled'    => 1,
+        'perms'         => 3,
     )
 );
 ?>
