79147769c5527577081c6cecc6bd7709cb92bd86
demian <demian@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-07-15 17:49:20 +0000

    cleaning up translation perms/role

--- modules/0.6/translation/data/navigation.php
+++ modules/0.6/translation/data/navigation.php
@@ -1,19 +1,4 @@
 <?php
-if (!defined('SGL_ROLE_TRANSLATOR')) {
-    /**
-     * Translator role.
-     *
-     * @param integer
-     */
-    define('SGL_ROLE_TRANSLATOR', 3);
-
-    /**
-     * Root node for translator's navigation branch.
-     *
-     * @param integer
-     */
-    define('SGL_NODE_TRANSLATOR', 5);
-}
 $aSections = array(
 
     /**
@@ -40,32 +25,6 @@ $aSections = array(
         'add_params'    => '',
         'is_enabled'    => 1,
         'perms'         => 'SGL_ADMIN',
-    ),
-
-    /**
-     * Nodes for translator navigation branch.
-     */
-    array(
-        'title'         => 'Manage Translations',
-        'parent_id'     => SGL_NODE_TRANSLATOR,
-        'uriType'       => 'dynamic',
-        'module'        => 'translation',
-        'manager'       => 'TranslationMgr.php',
-        'actionMapping' => '',
-        'add_params'    => '',
-        'is_enabled'    => 1,
-        'perms'         => SGL_ROLE_TRANSLATOR,
-    ),
-    array(
-        'title'         => 'Summary',
-        'parent_id'     => SGL_NODE_GROUP,
-        'uriType'       => 'dynamic',
-        'module'        => 'translation',
-        'manager'       => 'TranslationMgr.php',
-        'actionMapping' => 'summary',
-        'add_params'    => '',
-        'is_enabled'    => 1,
-        'perms'         => SGL_ROLE_TRANSLATOR,
     )
 );
 ?>
