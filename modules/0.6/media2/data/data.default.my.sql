45b7f99f172c82644e4ef2c1b942936fd1380c88
lakiboy <lakiboy@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-08-11 12:17:04 +0000

    better sample data strat

--- modules/0.6/media2/data/data.default.my.sql
+++ modules/0.6/media2/data/data.default.my.sql
@@ -3,3 +3,17 @@ INSERT INTO module VALUES ({SGL_NEXT_ID}, 1, 'media2', 'Media2', NULL, NULL, NUL
 -- SELECT @moduleId := MAX(module_id) FROM module;
 -- SELECT @memberId := 2;
 -- SELECT @anyRole  := -2;
+
+/*==============================================================*/
+/* Table: media_mime                                            */
+/*==============================================================*/
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'gif image', 'gif', 'image/gif', '47 49 46 38');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'jpeg image', 'jpg', 'image/jpeg', 'FF D8 FF');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'png image', 'png', 'image/png', '89 50 4E 47 0D 0A 1A 0A 00 00 00 0D 49 48 44 52');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'swf flash', 'swf', 'application/x-shockwave-flash', '46 57 53');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'flv video', 'flv', 'video/x-flv', '46 4C 56 01');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'pdf document', 'pdf', 'application/pdf', '25 50 44 46 2D 31 2E');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'flv video', 'doc', 'application/msword', 'D0 CF 11 E0 A1 B1 1A E1');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'zip archive', 'zip', 'application/zip', '50 4B 03 04');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'mpeg video', 'mpg', 'video/mpeg', '00 00 01 BA 21 00 01');
+INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'plain file', 'txt', 'text/plain', '');
--- modules/0.6/media2/data/data.sample.my.sql
+++ modules/0.6/media2/data/data.sample.my.sql
@@ -6,20 +6,6 @@ INSERT INTO `media_type` VALUES ({SGL_NEXT_ID}, 'album', 'album image');
 SELECT @mediaTypeId_profile := `media_type_id` FROM `media_type` WHERE `name` = 'profile';
 SELECT @mediaTypeId_album := `media_type_id` FROM `media_type` WHERE `name` = 'album';
 
-/*==============================================================*/
-/* Table: media_mime                                            */
-/*==============================================================*/
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'gif image', 'gif', 'image/gif', '47 49 46 38');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'jpeg image', 'jpg', 'image/jpeg', 'FF D8 FF');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'png image', 'png', 'image/png', '89 50 4E 47 0D 0A 1A 0A 00 00 00 0D 49 48 44 52');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'swf flash', 'swf', 'application/x-shockwave-flash', '46 57 53');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'flv video', 'flv', 'video/x-flv', '46 4C 56 01');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'pdf document', 'pdf', 'application/pdf', '25 50 44 46 2D 31 2E');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'flv video', 'doc', 'application/msword', 'D0 CF 11 E0 A1 B1 1A E1');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'zip archive', 'zip', 'application/zip', '50 4B 03 04');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'mpeg video', 'mpg', 'video/mpeg', '00 00 01 BA 21 00 01');
-INSERT INTO `media_mime` VALUES ({SGL_NEXT_ID}, 'plain file', 'txt', 'text/plain', '');
-
 SELECT @mediaMimeId_gif := `media_mime_id` FROM `media_mime` WHERE `name` = 'gif image';
 SELECT @mediaMimeId_jpg := `media_mime_id` FROM `media_mime` WHERE `name` = 'jpeg image';
 SELECT @mediaMimeId_png := `media_mime_id` FROM `media_mime` WHERE `name` = 'png image';
