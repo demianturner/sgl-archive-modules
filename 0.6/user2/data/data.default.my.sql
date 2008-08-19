ebf868e2969ba4a0d8746645079bd397f6aea4b6
Dmitri Lakachauskis <lakiboy83@gmail.com>
2008-08-19 10:54:12 +0000

    user2 updates

--- 0.6/user2/data/data.default.my.sql
+++ 0.6/user2/data/data.default.my.sql
@@ -1,3 +1,11 @@
 INSERT INTO module VALUES ({SGL_NEXT_ID}, 1, 'user2', 'User2', NULL, NULL, NULL, 'Dmitri Lakachauskis', NULL, NULL, NULL);
 
 SELECT @moduleId := MAX(module_id) FROM module;
+SELECT @memberId := 2;
+
+--
+-- Add permissions
+--
+INSERT INTO `permission` VALUES ({SGL_NEXT_ID}, 'account2mgr', '', @moduleId);
+SELECT @permissionId := `permission_id` FROM `permission` WHERE `name` = 'account2mgr';
+INSERT INTO `role_permission` VALUES ({SGL_NEXT_ID}, @memberId, @permissionId);
