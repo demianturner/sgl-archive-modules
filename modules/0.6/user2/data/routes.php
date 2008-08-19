64255c70921a5353ea7ee0c4148461508b55f2f4
lakiboy <lakiboy@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-08-19 09:21:49 +0000

    added user2 module

---
+++ modules/0.6/user2/data/routes.php
@@ -0,0 +1,19 @@
+<?php
+$aRoutes = array(
+    array('login', array(
+        'moduleName' => 'user2',
+        'controller' => 'login2',
+        'action'     => 'login'
+    )),
+    array('register', array(
+        'moduleName' => 'user2',
+        'controller' => 'login2',
+        'action' => 'register'
+    )),
+    array('logout', array(
+        'moduleName' => 'user2',
+        'controller' => 'login2',
+        'action' => 'logout'
+    ))
+);
+?>
---
+++ modules/0.6/user2/lang/email/english-utf-8.php
@@ -0,0 +1,16 @@
+<?php
+$words['__SGL_CATEGORY_welcome_new_user'] = 'Welcome new user';
+$words['email_signature_welcome_user'] = 'Registration';
+$words['email_subject_welcome_user'] = 'Welcome';
+$words['welcome %membername%'] = 'Welcome %membername%!';
+$words['username is %username%'] = 'Your username is %username%';
+$words['password is %password%'] = 'Your password is %password%';
+$words['go to site %url%'] = 'URL: %url%';
+$words['__SGL_CATEGORY_password_recovery'] = 'Password recovery';
+$words['email_signature_password_recovery'] = 'Reset Password';
+$words['email_subject_password_recovery'] = 'Reset Your Password';
+$words['dear %membername%'] = 'Dear %membername%';
+$words['we received your request to reset your password'] = 'We have received your request to reset your password.';
+$words['if it was not you - ignore'] = 'If this wasn\'t you, there\'s nothing to worry about - simply ignore this email and nothing will change.';
+$words['use this url to reset your password'] = 'Please use this URL [1] to reset your password within 5 days.';
+?>
