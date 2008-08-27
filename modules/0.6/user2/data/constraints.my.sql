a45a2fcafbac7ecd1783dc76f3c407e31bf4d2f9
lakiboy <lakiboy@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-08-27 10:34:35 +0000

    updated constraint.my.sql

--- modules/0.6/user2/data/constraints.my.sql
+++ modules/0.6/user2/data/constraints.my.sql
@@ -8,6 +8,10 @@ ALTER TABLE `user_passwd_hash`
 /*==============================================================*/
 /* Table: usr                                                   */
 /*==============================================================*/
-ALTER TABLE `usr`
+ALTER TABLE `user-address`
     ADD FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`)
-    ON DELETE SET NULL ON UPDATE SET NULL;
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `user-address`
+    ADD FOREIGN KEY (`usr_id`) REFERENCES `usr` (`usr_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE;
