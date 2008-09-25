71c8e0c31d1a6b4713294ce6c02006bdf3d3983d
Dmitri Lakachauskis <lakiboy83@gmail.com>
2008-09-25 10:57:55 +0000

    added password recovery routes

--- 0.6/user2/data/routes.php
+++ 0.6/user2/data/routes.php
@@ -15,5 +15,14 @@ $aRoutes = array(
         'controller' => 'login2',
         'action' => 'logout'
     )),
+    array('password/reset', array(
+        'moduleName' => 'user2',
+        'controller' => 'passwordrecovery'
+    )),
+    array('password/reset/:userId/:k', array(
+        'moduleName' => 'user2',
+        'controller' => 'passwordrecovery',
+        'action' => 'reset'
+    )),
 );
 ?>
