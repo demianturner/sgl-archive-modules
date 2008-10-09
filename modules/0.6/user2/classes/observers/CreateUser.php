6405482f3e0cdbe8850c24987812253cf9d168f6
lakiboy <lakiboy@7b896871-5dfa-0310-b5bf-cf0befd2132d>
2008-10-09 13:54:28 +0000

    moved some user1 prefs functionality to user2

--- modules/0.6/user2/classes/observers/CreateUser.php
+++ modules/0.6/user2/classes/observers/CreateUser.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once SGL_CORE_DIR . '/Emailer2.php';
-require_once SGL_CORE_DIR . '/Delegator.php';
-require_once SGL_MOD_DIR . '/user/classes/UserDAO.php';
 require_once SGL_MOD_DIR . '/user2/classes/User2DAO.php';
 require_once 'Text/Password.php';
 
@@ -16,9 +14,7 @@ class CreateUser extends SGL_Observer
 {
     public function __construct()
     {
-        $this->da = new SGL_Delegator();
-        $this->da->add(User2DAO::singleton());
-        $this->da->add(UserDAO::singleton());
+        $this->da = User2DAO::singleton();
     }
 
     public function update($observable)
@@ -34,24 +30,28 @@ class CreateUser extends SGL_Observer
         $observable->input->user->email = SGL_Emailer2::cleanMailInjection(
             $observable->input->user->email);
 
-        // @todo removeit
-        $observable->input->userId = null;
+        $errorCount = SGL_Error::count();
 
+        $aPrefs = array(
+            'language' => SGL::getCurrentLang() . '-' . SGL::getCurrentCharset()
+        );
         $aFields = array(
             'username'   => $observable->input->user->username,
             'passwd'     => $observable->input->user->passwd,
             'email'      => $observable->input->user->email,
             'first_name' => $observable->input->user->first_name,
             'last_name'  => $observable->input->user->last_name,
-            'created_by' => SGL_ADMIN,
         );
         $userId = $this->da->addUser($aFields);
+        $ok     = $this->da->addMasterPrefsByUserId($userId, $aPrefs);
 
-        if (!PEAR::isError($userId)) {
-            $observable->input->userId = $userId;
-            $aPrefs['language'] = SGL::getCurrentLang() . '-' . SGL::getCurrentCharset();
-            $ok = $this->_createPreferences($userId, $aPrefs);
+        // we need to commit transaction if no errors happened
+        if (SGL_Error::count() == $errorCount) {
+            $this->da->dbh->commit();
         }
+
+        $observable->input->userId = $userId;
+
         return $ok;
     }
 
@@ -59,22 +59,6 @@ class CreateUser extends SGL_Observer
     {
         $oPassword = new Text_Password();
         return $oPassword->create();
-    }
-
-    private function _createPreferences($userId, array $aUserPrefs)
-    {
-        $aPrefs    = $this->da->getMasterPrefs(SGL_RET_ID_VALUE);
-        $aPrefsMap = $this->da->getPrefsMapping();
-        foreach ($aUserPrefs as $prefName => $prefValue) {
-            $prefId = $aPrefsMap[$prefName];
-            $aPrefs[$prefId] = $prefValue;
-        }
-        if (!PEAR::isError($aPrefs)) {
-            $ok = $this->da->addPrefsByUserId($aPrefs, $userId);
-        } else {
-            $ok = $aPrefs;
-        }
-        return $ok;
     }
 }
 ?>
