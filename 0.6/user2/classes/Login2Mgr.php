44ed8bbedbf5aa827d36fe44ca0570220c691d6f
Dmitri Lakachauskis <lakiboy83@gmail.com>
2009-02-20 13:49:11 +0000

    removed iphone presence from user2

--- 0.6/user2/classes/Login2Mgr.php
+++ 0.6/user2/classes/Login2Mgr.php
@@ -81,13 +81,6 @@ class Login2Mgr extends SGL_Manager
 
         $output->pageTitle = 'Register';
         $output->template  = 'login2Register.html';
-
-        if (isset($_SERVER['HTTP_USER_AGENT'])
-            && strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'iphone') !== false)
-        {
-            $output->template = 'mobile_login2Register.html';
-            $output->mobile   = true;
-        }
     }
 
     public function _cmd_login(SGL_Registry $input, SGL_Output $output)
