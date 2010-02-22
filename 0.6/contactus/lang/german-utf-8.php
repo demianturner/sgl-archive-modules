155961e0cf40b2d8d9d43efd9b108628196f43b6
Demian Turner <demian@seagullsystems.com>
2010-02-22 15:15:37 +0000

    DE updates

--- 0.6/contactus/lang/german-utf-8.php
+++ 0.6/contactus/lang/german-utf-8.php
@@ -1,4 +1,30 @@
 <?php
+    $words = array(
+/*  CONTACT US MGR  */
+        'Contact Us' => 'Treten Sie mit uns in Kontakt',
+        'First Name' => 'Vorname',
+        'Last Name' => 'Nachname',
+        'Email' => 'Email',
+        'contact intro' => 'Füllen Sie bitte die folgenden Felder aus und drücken Sie auf "Absenden"',
+        'Type of enquiry' => 'Art der Anfrage',
+        'Comment' => 'Kommentar',
+        'Contact Enquiry from' => 'Kontaktanfrage von',
+        'aContactType' => array(
+            'General enquiry' => 'Allgemeine Anfrage',
+            'Get a quote' => 'Get a quote',
+            'Hosting info' => 'Hosting info',
+            'Site feedback' => 'Feedback',
+            ),
+
+/*  ERROR MSGS  */
+        'You must enter your first name' => 'Bitte geben Sie Ihren Vornamen an',
+        'You must enter your last name' => 'Bitte geben Sie Ihren Nachnamen an',
+        'You must fill in your comment' => 'Bitte geben Sie Ihren Kommentar ein ',
+        'Your email is not correctly formatted' => 'Ihre Email-Adresse fehlerhaft',
+        'You must enter your email' => 'Bitte geben Sie Ihre Email-Adresse an.',
+    );
+?>
+<?php
 $words['Contact Us'] = 'Treten Sie mit uns in Kontakt';
 $words['First Name'] = 'Vorname';
 $words['Last Name'] = 'Nachname';
