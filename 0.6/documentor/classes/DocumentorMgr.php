81512cb4315bb3d27e1b55c05445d618acd6fd83
Demian Turner <demian@seagullsystems.com>
2010-02-22 17:42:13 +0000

    updating copyright year -2

--- 0.6/documentor/classes/DocumentorMgr.php
+++ 0.6/documentor/classes/DocumentorMgr.php
@@ -1,6 +1,6 @@
 <?php
 // +---------------------------------------------------------------------------+
-// | Copyright (c) 2008, Demian Turner                                         |
+// | Copyright (c) 2010, Demian Turner                                         |
 // | All rights reserved.                                                      |
 // |                                                                           |
 // | Redistribution and use in source and binary forms, with or without        |
